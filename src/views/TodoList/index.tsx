29470525806f8b9e50b6a7551645d2cc5b2c80a7
qigeniuX <526978663@qq.com>
2022-08-28 19:51:24 +0800

    TODOLIST完成添加状态、时间栏、card等

--- src/views/TodoList/index.tsx
+++ src/views/TodoList/index.tsx
@@ -1,16 +1,15 @@
-import { Layout, Table, Button, Input, Popover } from "antd"
+import { Layout, Table, Button, Input, Card, DatePicker } from "antd"
 import { Content,  Header } from "antd/lib/layout/layout"
 import moment from "moment"
 import React, { useEffect, useState } from "react"
-import { render } from "react-dom"
 import { v4 as uuidv4 } from 'uuid'
 import { cloneDeep } from 'lodash-es'
 
-// TODO: 添加一个状态 （仅仅是新任务list）表示是新增的还是撤回的
-// TODO: 添加一个“上次修改时间” 修改后展示修改成功的时间 如果是添加或者撤回则显示添加或者撤回时间
+// TODO: 添加一个状态 （仅仅是新任务list）表示是新增的还是撤回的 done
+// TODO: 添加一个“上次修改时间” 修改后展示修改成功的时间 如果是添加或者撤回则显示添加或者撤回时间 
  
 // 比较拓展的TODO
-// TODO: 添加一个card 里面是快捷任务 点一下就可以进入未完成任务 输入input右边增加一个按钮（添加快捷任务） 点击后会进入card
+// TODO: 添加一个card 里面是快捷任务 点一下就可以进入未完成任务 输入input右边增加一个按钮（添加快捷任务） 点击后会进入card  done
 // TODO: 添加一个输入框 输入任务的期望完成时间 分两种 1. 时间跨度 （如 下周一 ~ 下周日） 2. 时间到期 （如24小时之后到期）
 // TODO: 提前完成需求的展示 未完成需求的展示
 
@@ -21,13 +20,14 @@ const TodoList = () => {
 	const [finishedTaskData, setFinishedTaskData] = useState<any[]>([])
   const [inputValue, setInputValue] = useState<string>('')
   const [modifyInputValue, setModifyInputValue] = useState<string>('')
+	const [cardValue, setCardValue] = useState<string>('点我添加日常任务')
 
 	const newTaskColumns = [
 		{
 			title: '未完成任务',
 			dataIndex: 'theTask',
 			key: 'theTask',
-      width: 280,
+      width: '30%',
       ellipsis: true,
 			render: (text:any, record: any, index: number) => (
         record.isEditing ?
@@ -50,15 +50,29 @@ const TodoList = () => {
 			title: '添加时间',
 			dataIndex: 'theTime',
 			key: 'theTime',
+			width: '15%',
+		},
+		{
+			title: '计划完成日期',
+			dataIndex: 'deadlineDate',
+			key: 'deadlineDate',
+			width: '15%',
+			render: () => (
+				<DatePicker
+					allowClear
+				/>
+			)
 		},
     {
       title: '来自',
       dataIndex: 'state',
-      key: 'state'
+      key: 'state',
+			width: '10%',
     },
 		{
 			title: '操作',
 			key: 'actions',
+			width: '25%',
 			render:(text:any, record:any, index: number) => (
 				<>
           {
@@ -71,7 +85,7 @@ const TodoList = () => {
               <>
                 <Button onClick={() => handleModifyButtonClick(index)}>修改</Button>
                 <Button danger onClick={() => handleClickDeleteButton(index)}>删除</Button>
-			      		<Button type="primary" onClick={() => handleClickFinishedButton(index)}>完成</Button> 
+								<Button type="primary" onClick={() => handleClickFinishedButton(index)}>完成</Button> 
               </>
 
             )
@@ -88,17 +102,19 @@ const TodoList = () => {
 			dataIndex: 'theTask',
 			key: 'theTask',
       ellipsis: true,
-      width: 160,
+      width: '30%',
       render: (text :string) => <span style={{ textDecoration: 'line-through' }}>{text}</span>
 		},
 		{
 			title: '完成时间',
 			dataIndex: 'theTime',
+			width: '15%',
 			key: 'theTime'
 		},
 		{
 			title: '操作',
 			key: 'actions',
+			width: '25%',
 			render:(text:any, record:any, index:number) => (
 				<>
 					<Button onClick={() => handleClickRevokeButton(index)}>撤销</Button>
@@ -131,6 +147,7 @@ const TodoList = () => {
 		const finData = [...finishedTaskData]
 		const revokeData = finData.splice(index,1)
 		revokeData[0].theTime = moment().format('YYYY-MM-DD HH:mm:ss')
+		revokeData[0].state = "撤回"
 
 		newData.push(...revokeData)
 
@@ -180,12 +197,13 @@ const TodoList = () => {
     const newData = [...taskData]
     newData[index].isEditing = false
     newData[index].theTask = modifyInputValue
+		newData[index].state = '修改'
 
     setTaskData(newData)
 
     const dataForSave = cloneDeep(newData)
     dataForSave.forEach((ele) => {
-      delete ele.isEditing
+    delete ele.isEditing
     })
     
     localStorage.setItem('todo_list__unfinished_tasks', JSON.stringify(dataForSave))
@@ -206,6 +224,38 @@ const TodoList = () => {
 
   }
 
+	const handleAddCardTaskButton = () => {
+		setCardValue(inputValue)
+		console.log(cardValue)
+
+		localStorage.setItem('daily_card_task',cardValue)
+	}
+
+	const handleCardClick = () => { 
+		if (cardValue === '') {return}
+
+		const newTask = [...taskData]
+		
+		const theKey = uuidv4()
+		
+		newTask.push({
+			key: theKey,
+			theTask: cardValue,
+			state: '日常任务',
+			theTime: moment().format('YYYY-MM-DD HH:mm:ss'),
+		})
+
+		const dataForSave = cloneDeep(newTask)
+    dataForSave.forEach((ele) => {
+      delete ele.isEditing
+    })
+
+		setTaskData(newTask)
+
+		localStorage.setItem('daily_card_task',cardValue)
+    localStorage.setItem('todo_list__unfinished_tasks', JSON.stringify(dataForSave))
+	}
+  
   // TODO: 添加类型
 	const addDatum = (value: any) => {
 		if (value === '') { return }
@@ -216,7 +266,9 @@ const TodoList = () => {
 		addTaskData.push({
       key: theKey,
       theTask: value,
-      theTime: moment().format('YYYY-MM-DD HH:mm:ss')
+			state: '新增',
+      theTime: moment().format('YYYY-MM-DD HH:mm:ss'),
+
     })
 
 		setTaskData(addTaskData)
@@ -247,8 +299,12 @@ const TodoList = () => {
     if ( localStorage.getItem('todo_list__finished_tasks') === null )
       localStorage.setItem('todo_list__finished_tasks', '[]')
 
+		if ( localStorage.getItem('daily_card_task') === null )
+      localStorage.setItem('daily_card_task', '')
+
     setTaskData(JSON.parse(localStorage.getItem('todo_list__unfinished_tasks')!) as string[])
     setFinishedTaskData(JSON.parse(localStorage.getItem('todo_list__finished_tasks')!) as string[])
+		setCardValue(localStorage.getItem('daily_card_task')!)
   }, [])
 
 	return (
@@ -267,12 +323,21 @@ const TodoList = () => {
           onChange={handleInputSearchChange}
           allowClear
 				/>
+				<Button
+					onClick={handleAddCardTaskButton}
+				>点我添加为日常任务</Button>
+				
 			</Header>
 
 			<Layout>
 				<Content>
 						<Table columns={newTaskColumns} dataSource={taskData} />
-
+						<Card 
+							style={{width: '250px', margin: '15px'}}
+							hoverable={true}
+							onClick={handleCardClick}
+							
+						>{cardValue}</Card>
 						<Table
               columns={finishedTaskColumns} 
 						  dataSource={finishedTaskData} 
@@ -283,6 +348,7 @@ const TodoList = () => {
 						// }
 						/>
 				</Content>
+			
 			</Layout>
 		</Layout>
 	)
